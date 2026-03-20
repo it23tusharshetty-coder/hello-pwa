6b2e74a6d10b8c4fa795536ae86dd043d7122f80
it23tusharshetty-coder <it23.tushar.shetty@kccemsr.edu.in>
2026-03-20 11:34:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,9 +12,10 @@ caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
 });
 // Fetch event: Serve from cache if offline
 self.addEventListener('fetch', (event) => {
-event.respondWith(
-caches.match(event.request).then((response) => {
-return response || fetch(event.request);
-})
-);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match('/index.html'))
+  );
+});
 });
